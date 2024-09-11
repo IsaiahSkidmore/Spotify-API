c90cdf295fa4e8d8945db42879f2caf17c05f209
haydencfb <haydenfitzbrintle@gmail.com>
2024-09-10 18:23:34 -0700

    update

---
+++ spotify-api-client/src/components/Search.jsx
@@ -0,0 +1,11 @@
+import classes from '../styles/Search.module.css';
+
+function Search() {
+    return (
+        <div className={classes.search}>
+        <h1 className={`${classes.searchH1}`}>Search</h1>
+        </div>
+    );
+}
+
+export default Search;
